78da79d916013b3025ac4123b6e4bf0e28905c97
Rimantas <belauzas@gmail.com>
2025-05-22 09:39:11 +0300

    Basketball DOM elements

--- js/krepsinis.js
+++ js/krepsinis.js
@@ -29,3 +29,7 @@ appDOM.innerHTML = `
         <div class="entry away">Antra komanda pelne 3 taskus.</div>
     </div>
 `;
+
+const [homeResultDOM, awayResultDOM] = appDOM.querySelectorAll('.result');
+const [home1, home2, home3, away1, away2, away3] = appDOM.querySelectorAll('button');
+const historyDOM = appDOM.querySelector('.history');
